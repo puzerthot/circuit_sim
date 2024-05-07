2ff60e51b60f43f5b889ed7bf774af528a54b291
Paul Cheuk <paulcheuk@bluefield.com.hk>
2024-05-07 16:41:52 +0800

    1

--- circuit_sim/Form1.cs
+++ circuit_sim/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,15 +19,56 @@ namespace circuit_sim
 
         private void Work()
         {
-            var tmp = new NetList("C:\\Users\\paulcheuk\\Desktop\\Playground\\circuit_sim\\circuit_sim\\bin\\Debug\\net1.txt");
+            //var tmp = new NetList("C:\\Users\\paulcheuk\\Desktop\\Playground\\circuit_sim\\circuit_sim\\bin\\Debug\\net1.txt");
+            //var config = new NetList.SimulationConfig();
+            //var c = tmp.Simulation(config);
+
+            var tmp = new NetList("C:\\Users\\paulcheuk\\Desktop\\Playground\\circuit_sim\\circuit_sim\\bin\\Debug\\net2.txt");
             var config = new NetList.SimulationConfig();
+            config.SetTemperatureInDegree(27);
             var c = tmp.Simulation(config);
 
-            //var tmp = new NetList("C:\\Users\\paulcheuk\\Desktop\\Playground\\circuit_sim\\circuit_sim\\bin\\Debug\\net2.txt");
+
+            //var dio = new NetList.DiodeBranch("D3");
             //var config = new NetList.SimulationConfig();
             //config.SetTemperatureInDegree(27);
-            //var c = tmp.Simulation(config);
+            //dio.PrepareFor(config);
+            //dio.SetValue("Eta:0.9999;Isat:1E-14;");
+            //dio.finalC = 12.025;
+
+
+            //double r = 100;
+            //var g = 1 / r;
+            //var vs = 55;
+
+            //var errorFunc = new Func<double, double>((vn) => vs * g - vn * g - dio.CurrentGivenVd(vn));
+            //var dErrorFunc = new Func<double, double>((vn) => -g - dio.CurrentDerivativeGivenVd(vn));
+
+            //var finalValue = NewtonMethod(0, errorFunc, dErrorFunc);
+
+            //Console.WriteLine($"final:{finalValue}, error:{errorFunc(finalValue)}, current:{dio.CurrentGivenVd(finalValue)}");
+
+            foreach(var item in c)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
         }
 
+        public double NewtonMethod(double init, Func<double, double> error, Func<double, double> derivative, double eplison = 1e-9)
+        {
+            double ak = init;
+            for (int i = 0; i < 10000; i++)
+            {
+                var errorValue = error(ak);
+                var derivativeValue = derivative(ak);
+                ak -= errorValue / derivativeValue;
+
+                if (Math.Abs(errorValue) < eplison)
+                {
+                    break;
+                }
+            }
+            return ak;
+        }
     }
 }
--- circuit_sim/NetList.cs
+++ circuit_sim/NetList.cs
@@ -3,39 +3,90 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Windows.Forms;
 
 
 namespace circuit_sim
 {
     class NetList
     {
-        class DiodeBranch : Branch
+        public class DiodeBranch : Branch
         {
-            public const double q = 1.60217663e-19;// charge on an electron: 1.60217663 × 10-19 coulombs
+            public override bool IsNonlinear => true;
+            public const double q = -1.60217663e-19;// charge on an electron: 1.60217663 × 10-19 coulombs
             public const double k = 1.380649e-23; //Boltzman's constant: 1.380649 × 10-23 m2 kg s-2 K-1
             public double T; //absolute temperature in degree Kelvin
             public double Eta; //non-ideality factor
             public double Isat; //reverse saturation current;
+
+            public double finalC;
+            public bool finalCLock = false;
             public DiodeBranch(string id) : base(id)
             {
             }
             public double CurrentGivenVd(double vd)
             {
                 // ref: circuit and system simulation methods.pdf, page 24
-                var current = Isat * (Math.Exp(q * vd / (Eta * k * T)) - 1);
+                var current = Isat * (Math.Exp(finalC * vd) - 1);
                 return current;
+            }
+            public double CurrentDerivativeGivenVd(double vd)
+            {
+                return Isat * Math.Exp(finalC * vd) * finalC;
+            }
+            public override void PrepareFor(SimulationConfig simulationConfig)
+            {
+                T = simulationConfig.TemperatureInDegreeKelvin;
+                if (!finalCLock)
+                {
+                    finalC = q / (Eta * k * T);
+                }
+            }
+
+            public override (double Ieq, double Geq) LinearizedModel(double vd)
+            {
+                var id = CurrentGivenVd(vd);
+                var Geq = CurrentDerivativeGivenVd(vd);
+                var Ieq = id - vd * Geq;
+                return (Ieq, Geq);
             }
             public override void SetValue(string valueString)
             {
-                base.SetValue(valueString);
+                var values = valueString.Split(';');
+                foreach (var value in values)
+                {
+                    if (!string.IsNullOrEmpty(value.Trim()))
+                    {
+                        var items = value.Split(':');
+                        var key = items[0];
+                        var rkey = key.ToLower().Trim();
+                        switch (rkey)
+                        {
+                            case "isat":
+                                Isat = double.Parse(items[1]);
+                                break;
+                            case "eta":
+                                Eta = double.Parse(items[1]);
+                                break;
+                            case "finalc":
+                                finalC = double.Parse(items[1]);
+                                finalCLock = true;
+                                break;
+                            default:
+                                throw new InvalidDataException($"Diode Dont have a value for key: {key}");
+                        }
+                    }
+                }
             }
         }
-        class Branch
+        public class Branch
         {
+            public virtual bool IsNonlinear => false;
             public Branch(string id)
             {
                 ID = id;
             }
+            public virtual void PrepareFor(SimulationConfig simulationConfig) { }
             public enum BranchTypeEnum { Current, Resistor, Voltage, Diode }
             public BranchTypeEnum BranchType;
             public string ID { get; private set; }
@@ -46,7 +97,10 @@ namespace circuit_sim
             {
                 Value = double.Parse(valueString);
             }
-
+            public virtual (double Ieq, double Geq) LinearizedModel(double Vd)
+            {
+                throw new NotSupportedException($"Branch({BranchType}) Not Support Linearization");
+            }
             public static Branch NewBranchOf(string nodeId)
             {
                 switch (nodeId[0].ToString().ToUpper())
@@ -115,73 +169,103 @@ namespace circuit_sim
         }
         public Dictionary<int, double> Simulation(SimulationConfig config)
         {
-            var Y = new pMatrix(NodeIDDict.Count);
-            var J = pVector.NewWithLen(NodeIDDict.Count);
             foreach (var b in Branches)
             {
-                //ref: circuit and system simulation methods.pdf,  page 19, 
-                var i = NodeIDDict[b.FromNode].Index;
-                var j = NodeIDDict[b.ToNode].Index;
-                if (b.BranchType == Branch.BranchTypeEnum.Resistor)
-                {
-                    var conductance = 1 / b.Value;
-                    Y[i, i] += conductance;
-                    Y[i, j] -= conductance;
-                    Y[j, i] -= conductance;
-                    Y[j, j] += conductance;
-                }
-                else if (b.BranchType == Branch.BranchTypeEnum.Current)
-                {
-                    J[i] -= b.Value;
-                    J[j] += b.Value;
-                }
+                b.PrepareFor(config);
             }
-
-            //to definiate admitance, ref: circuit and system simulation methods.pdf,  page 21
             int selectGroudNodeIndex = 0;
-            var rY = new pMatrix(Y.Rows - 1, Y.Columns - 1);
-            foreach (var (r, c) in Y.Indexes())
+            bool isAnyNonlinear = Branches.Any(k => k.IsNonlinear);
+            bool isConverge = false;
+            var Vi = pVector.NewWithLen(NodeIDDict.Count);
+            var random = new Random(0);
+            do
             {
-                if (r != selectGroudNodeIndex && c != selectGroudNodeIndex)
+                var Y = new pMatrix(NodeIDDict.Count);
+                var J = pVector.NewWithLen(NodeIDDict.Count);
+                foreach (var b in Branches)
                 {
-                    var tr = r > selectGroudNodeIndex ? r - 1 : r;
-                    var tc = c > selectGroudNodeIndex ? c - 1 : c;
-                    rY[tr, tc] = Y[r, c];
-                }
-            }
+                    //ref: circuit and system simulation methods.pdf,  page 19, 
+                    var i = NodeIDDict[b.FromNode].Index;
+                    var j = NodeIDDict[b.ToNode].Index;
+                    if (b.BranchType == Branch.BranchTypeEnum.Resistor)
+                    {
+                        var conductance = 1 / b.Value;
+                        Y[i, i] += conductance;
+                        Y[i, j] -= conductance;
+                        Y[j, i] -= conductance;
+                        Y[j, j] += conductance;
+                    }
+                    else if (b.BranchType == Branch.BranchTypeEnum.Current)
+                    {
+                        J[i] -= b.Value;
+                        J[j] += b.Value;
+                    }
+                    else if (b.IsNonlinear)
+                    {
+                        var (Ieq, Geq) = b.LinearizedModel(Vi[i] - Vi[j]);
+                        if (Ieq != 0.0)
+                        {
+                            J[i] -= Ieq;
+                            J[j] += Ieq;
+                        }
 
-            var rJ = pVector.NewWithLen(J.Size - 1);
-            for (int i = 0; i < J.Size; i++)
-            {
-                var tj = i > selectGroudNodeIndex ? i - 1 : i;
-                if (i != selectGroudNodeIndex)
+                        if (Geq != 0.0)
+                        {
+                            Y[i, i] += Geq;
+                            Y[i, j] -= Geq;
+                            Y[j, i] -= Geq;
+                            Y[j, j] += Geq;
+                        }
+                    }
+                }
+
+                //to definiate admitance, ref: circuit and system simulation methods.pdf,  page 21
+                var rY = new pMatrix(Y.Rows - 1, Y.Columns - 1);
+                foreach (var (r, c) in Y.Indexes())
                 {
-                    rJ[tj] = J[i];
+                    if (r != selectGroudNodeIndex && c != selectGroudNodeIndex)
+                    {
+                        var tr = r > selectGroudNodeIndex ? r - 1 : r;
+                        var tc = c > selectGroudNodeIndex ? c - 1 : c;
+                        rY[tr, tc] = Y[r, c];
+                    }
                 }
-            }
 
-            var rV = pMatrix.SolveViaLUDecomposition(rY, rJ);
-
-            var test = rY * rV;
-
-            var v = pVector.NewWithLen(J.Size);
-            for (int i = 0; i < J.Size; i++)
-            {
-                if (i == selectGroudNodeIndex)
-                {
-                    v[i] = 0;
-                }
-                else
+                var rJ = pVector.NewWithLen(J.Size - 1);
+                for (int i = 0; i < J.Size; i++)
                 {
                     var tj = i > selectGroudNodeIndex ? i - 1 : i;
-                    v[i] = rV[tj];
+                    if (i != selectGroudNodeIndex)
+                    {
+                        rJ[tj] = J[i];
+                    }
                 }
-            }
+
+                var rV = pMatrix.SolveViaLUDecomposition(rY, rJ);
+                double totalDiff = 0.0;
+                for (int i = 0; i < J.Size; i++)
+                {
+                    if (i == selectGroudNodeIndex)
+                    {
+                        Vi[i] = 0;
+                    }
+                    else
+                    {
+                        var tj = i > selectGroudNodeIndex ? i - 1 : i;
+                        totalDiff += Math.Pow(Vi[i] - rV[tj], 2);
+                        Vi[i] = rV[tj];
+
+                    }
+                }
+
+                isConverge = Math.Sqrt(totalDiff) < 1e-8;
+
+            } while (isAnyNonlinear && !isConverge);
 
             var tmp = new Dictionary<int, double>();
-            for (int i = 0; i < v.Size; i++)
+            for (int i = 0; i < Vi.Size; i++)
             {
-                var value = v[i];
+                var value = Vi[i];
                 var nodeId = NodeIDDict.Where(k => k.Value.Index == i).Single().Value.Index;
                 tmp[nodeId] = value;
             }
